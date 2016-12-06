eeb33c3128b8122bb5c47d655f15313622dc9cf4
Ben Myers <ben.myers@okstate.edu>
2016-12-05 23:14:21 -0600

    Day 6a complete

---
+++ day6/Day6a.java
@@ -0,0 +1,57 @@
+import java.util.*;
+import java.io.*;
+
+public class Day6a
+{
+    public static void main(String[] args)
+    {
+        Scanner scan = new Scanner("BOGUS DECLARATION TO APPEASE THE COMPILER GODS");
+        try
+        {
+            scan = new Scanner(new File("input.txt"));
+        } catch (Exception e)
+        {
+            e.printStackTrace();
+            System.exit(0);
+        }
+
+        String[] columns = new String[8];
+        Arrays.fill(columns, "");
+        while(scan.hasNextLine())
+        {
+            String line = scan.nextLine();
+            for(int i = 0; i < 8; i++) // the length of each line is 8
+            {
+                columns[i] += "" + line.charAt(i);
+            }
+        }
+
+        String solution = "";
+        for(int i = 0; i < 8; i++)
+        {
+            solution += getMostFrequentChar(columns[i] + "");
+        }
+
+        System.out.println(solution);
+    }
+
+    public static char getMostFrequentChar(String str)
+    {
+        char mostFrequent = ' ';
+        int maxFrequency = 0;
+        for(int i = 0; i < 26; i++)
+        {
+            int frequency = 0;
+            for(int j = 0; j < str.length(); j++)
+            {
+                if(str.charAt(j) == (char)(i + 'a')) { frequency++; }
+            }
+            if(frequency > maxFrequency)
+            {
+                maxFrequency = frequency;
+                mostFrequent = (char)(i + 'a');
+            }
+        }
+        return mostFrequent;
+    }
+}
